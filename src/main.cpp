fdad6764803eae89c6687df3cf1c7fb11af218ec
calebe94 <calebe.sep@hotmail.com>
2021-06-12 22:54:34 -0300

    Added can receiver program to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,69 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks. 
- *  The API is almost the same as with the WiFi Shield library, 
- *  the most obvious difference being the different file you need to include:
- */
+
 #include "Arduino.h"
-#include "ESP8266WiFi.h"
+#include <SPI.h>
+#include <mcp_can.h>
 
-void setup() 
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+
+#define CAN0_INT 2                              // Set INT to pin 2
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+
+void setup()
 {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    Serial.println("Inicializing MCP2515...");
 
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("Setup done");
-}
-
-void loop() 
-{
-  Serial.println("scan start");
-
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0)
-    Serial.println("no networks found");
-  else
-  {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i)
+    // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+    if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
     {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-      delay(10);
+        Serial.println("MCP2515 Initialized Successfully!");
     }
-  }
-  Serial.println("");
+    else
+    {
+        Serial.println("Error Initializing MCP2515...");
+    }
 
-  // Wait a bit before scanning again
-  delay(5000);
+    CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+
+    pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+    Serial.println("MCP2515 Library Receive Example...");
 }
+
+void loop()
+{
+    /*
+    if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
+    {
+        CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+
+        if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+        {
+            sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+        }
+        else
+        {
+            sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+        }
+
+        Serial.print(msgString);
+
+        if((rxId & 0x40000000) == 0x40000000)    // Determine if message is a remote request frame.
+        {
+            sprintf(msgString, " REMOTE REQUEST FRAME");
+            Serial.print(msgString);
+        }
+        else
+        {
+            for(byte i = 0; i<len; i++)
+            {
+                sprintf(msgString, " 0x%.2X", rxBuf[i]);
+                Serial.print(msgString);
+            }
+        }
+        Serial.println();
+    }*/
+}
+
