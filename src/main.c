98dffa6e74d3f657a1fc6854ee3a4be1af468e37
calebe94 <calebe.sep@hotmail.com>
2021-06-18 19:03:08 -0300

    Updated functions name

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/***************************
+ * INCLUDES
+****************************/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,36 +8,49 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
+/***************************
+ * DEFINE
+****************************/
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 #define GPIO_INPUT_IO_0     4
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0))
 #define ESP_INTR_FLAG_DEFAULT 0
+
+/***************************
+ * GLOBAL VARIABLES
+****************************/
 static xQueueHandle gpio_evt_queue = NULL;
 
-static void IRAM_ATTR gpio_isr_handler(void* arg)
+/***************************
+ * FUNCTIONS
+****************************/
+static void IRAM_ATTR flowsensor_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-static void gpio_task_example(void* arg)
+static void flowsensor_task(void* arg)
 {
-    uint32_t io_num;
-    for(;;) {
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+    uint32_t io_num = 0;
+    uint32_t pulses = 0;
+    for(;;)
+    {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        {
             printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
         }
     }
 }
 
+/***************************
+ * MAIN
+****************************/
 void app_main()
 {
     gpio_config_t io_conf;    
     //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
     //bit mask of the pins, use GPIO4/5 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     //set as input mode
@@ -54,24 +62,22 @@ void app_main()
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, flowsensor_isr_handler, (void*) GPIO_INPUT_IO_0);
 
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    xTaskCreate(flowsensor_task, "flowsensor_task", 2048, NULL, 10, NULL);
 
-    while(1) {
-        /* Blink off (output low) */
-        printf("Turning off the LED\n");
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        printf("Turning on the LED\n");
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    uint32_t xStart, xEnd, xDifference;
+    while(1)
+    {
+        xStart = xTaskGetTickCount();
+        vTaskDelay( pdMS_TO_TICKS( 1000UL ) );
+        xEnd = xTaskGetTickCount();
+        xDifference = xEnd - xStart;
+        printf("Time diff: %lu ticksn\n",(long unsigned int)pdTICKS_TO_MS(xDifference) );
     }
 }
