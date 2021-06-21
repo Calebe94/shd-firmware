d5d29588faaa4fcdc66b7907ea792400a6838176
calebe94 <calebe.sep@hotmail.com>
2021-06-21 16:10:17 -0300

    Added rs485 echo example to main

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "sensor/flowsensor.h"
+#include "rs485/rs485.h"
 
 /***************************
  * MAIN
@@ -16,9 +17,18 @@
 void app_main()
 {
     flowsensor_init();
+    rs485_init();
 
+    rs485_send("Teste de RS485 iniciado!");
     while(1)
     {
+        uint8_t data[30];
+        int len = rs485_read(data);
+        if(len > 0)
+        {
+            printf("Recebido: %s\n", (char*)data);
+            rs485_send("Recebido");
+        }
         printf("pulses: %d - litros: %f\n", flowsensor_get_pulses(), flowsensor_get_litros());
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
