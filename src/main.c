44f980b17d63026dc92ee47d77fcee84a6d1b49c
calebe94 <calebe.sep@hotmail.com>
2021-06-21 18:51:23 -0300

    Added console module

--- src/main.c
+++ src/main.c
@@ -2,24 +2,28 @@
  * INCLUDES
 ****************************/
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include "freertos/timers.h"
-#include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "esp_log.h"
+
+#include "serial/console.h"
 #include "sensor/flowsensor.h"
 #include "rs485/rs485.h"
+
 
 /***************************
  * MAIN
 ****************************/
 void app_main()
 {
+    console_init();
     flowsensor_init();
     rs485_init();
 
-    rs485_send("Teste de RS485 iniciado!");
+    //rs485_send("Teste de RS485 iniciado!");
     while(1)
     {
         uint8_t data[30];
@@ -27,7 +31,7 @@ void app_main()
         if(len > 0)
         {
             printf("Recebido: %s\n", (char*)data);
-            rs485_send("Recebido");
+            //rs485_send("Recebido");
         }
         printf("pulses: %d - litros: %f\n", flowsensor_get_pulses(), flowsensor_get_litros());
         vTaskDelay(pdMS_TO_TICKS(1000));
