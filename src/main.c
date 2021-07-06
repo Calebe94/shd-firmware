e239ca71a3c734dc4fa9680b20e102859c732177
calebe94 <calebe.sep@hotmail.com>
2021-07-05 21:25:40 -0300

    Fixed spiffs bug which can't open files

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@
 #include "protocol/protocol.h"
 #include "protocol/message_process.h"
 
+static const char * TAG = "MAIN";
+
 /***************************
  * MAIN
 ****************************/
@@ -40,6 +42,69 @@ void app_main()
     netbiosns_init();
     #endif
     ESP_ERROR_CHECK(init_flash_storage("/spiffs", "spiffs"));
+    
+    // Use POSIX and C standard library functions to work with files.
+    // First create a file.
+    ESP_LOGI(TAG, "Opening file");
+    FILE* f = fopen("/spiffs/hello.txt", "w");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return;
+    }
+    fprintf(f, "Hello World!\n");
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat("/spiffs/foo.txt", &st) == 0) {
+        // Delete it if it exists
+        unlink("/spiffs/foo.txt");
+    }
+
+    // Rename original file
+    ESP_LOGI(TAG, "Renaming file");
+    if (rename("/spiffs/hello.txt", "/spiffs/foo.txt") != 0) {
+        ESP_LOGE(TAG, "Rename failed");
+        return;
+    }
+
+    // Open renamed file for reading
+    ESP_LOGI(TAG, "Reading file");
+    f = fopen("/spiffs/foo.txt", "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+    char line[64];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+    // strip newline
+    char* pos = strchr(line, '\n');
+    if (pos)
+    {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
+    
+    ESP_LOGI(TAG, "Reading file");
+    f = fopen("/spiffs/settings.json", "rb");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);  
+
+    char *string = malloc(fsize + 1);
+    fread(string, 1, fsize, f);
+    fclose(f);
+
+    string[fsize] = 0;
+    
+    ESP_LOGI(TAG, "size: %d - content: %s", (int)fsize,string);
 
     while(1)
     {
