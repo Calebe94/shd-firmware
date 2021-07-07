369caaf96ebd18cc2fb181b2b600be2601cf07b1
calebe94 <calebe.sep@hotmail.com>
2021-07-07 16:28:27 -0300

    Added main.js and settings.html file routes

--- src/main.c
+++ src/main.c
@@ -41,8 +41,25 @@ esp_err_t init_routes(httpd_handle_t server, rest_server_context_t *rest_context
         .user_ctx = rest_context
     };
 
+    httpd_uri_t get_js_uri = {
+        .uri = "/main.js",
+        .method = HTTP_GET,
+        .handler = rest_common_get_handler,
+        .user_ctx = rest_context
+    };
+
+    httpd_uri_t get_settings_uri = {
+        .uri = "/settings.html",
+        .method = HTTP_GET,
+        .handler = rest_common_get_handler,
+        .user_ctx = rest_context
+    };
+
     httpd_register_uri_handler(server, &common_get_uri);
     httpd_register_uri_handler(server, &get_css_uri);
+    httpd_register_uri_handler(server, &get_settings_uri);
+    httpd_register_uri_handler(server, &get_js_uri);
+
     init_settings_routes(server, rest_context);
     return ESP_OK;
 }
