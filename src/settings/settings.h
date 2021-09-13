24b1f51f07abeda5fc9a0d3524e7cdb822b8bc69
calebe94 <calebe.sep@hotmail.com>
2021-09-13 18:32:11 -0300

    Changed phone key to be a list on json fil

--- src/settings/settings.h
+++ src/settings/settings.h
@@ -2,6 +2,7 @@
 #define _SETTINGS_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define SETTINGS_FILE "/spiffs/settings.json"
 
@@ -13,7 +14,7 @@ typedef enum {
 typedef struct settings {
     uint8_t id;
     settings_mode_t mode;
-    char phone[20]; // O máximo pode ser 18 com terminador nulo. 20 é o coeficiente de cagaço.
+    char phone[20][20]; // O máximo pode ser 18 com terminador nulo. 20 é o coeficiente de cagaço.
     char local[128];
     int interval;
 } settings_t;
@@ -26,9 +27,11 @@ uint8_t settings_get_id(void);
 
 void settings_set_id(uint8_t);
 
-void settings_set_phone(char*);
+bool settings_set_phone(uint8_t, char*);
 
-char *settings_get_phone(void);
+char *settings_get_phone(uint8_t);
+
+uint8_t settings_get_phones_list_length();
 
 settings_mode_t settings_get_mode(void);
 
