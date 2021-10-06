fff8168d6913d60c31c9b7e5b0a51d269214d823
calebe94 <calebe.sep@hotmail.com>
2021-10-06 13:27:22 -0300

    Added settings and devices handlers

--- webpage/routes_sim.py
+++ webpage/routes_sim.py
@@ -1,15 +1,41 @@
 from flask import Flask, request, render_template, send_from_directory
-import os
+import os, json
 
 file_path = os.path.realpath(__file__)
+data_path = os.path.dirname(file_path)
+
+###################################################################################################
+# Global data
+###################################################################################################
+global_settings = dict()
+global_devices  = dict()
+
+###################################################################################################
+# Miscellaneous
+###################################################################################################
+def load_settings():
+    settings_json = dict()
+    with open(os.path.join(data_path, "settings.json")) as settings_file:
+        settings_json = json.load(settings_file)
+    return settings_json
+
+def load_devices():
+    devices_json = dict()
+    with open(os.path.join(data_path, "devices.json")) as devices_file:
+        devices_json = json.load(devices_file)
+    return devices_json
+
+###################################################################################################
+# Flask Routes
+###################################################################################################
 app = Flask(__name__, 
-        static_url_path=os.path.dirname(file_path), 
-        static_folder=os.path.dirname(file_path), 
-        template_folder=os.path.dirname(file_path))
+        static_url_path=data_path,
+        static_folder=data_path,
+        template_folder=data_path)
 
 @app.route('/<path:path>')
 def send_static_file(path):
-    return send_from_directory(os.path.dirname(file_path), path)
+    return send_from_directory(data_path, path)
 
 @app.route('/readings.html', methods=['GET'])
 def readings_route():
@@ -25,18 +51,24 @@ def index():
 
 @app.route("/get/id", methods=['GET'])
 def get_id():
-    return '{"id": 1}'
+    return '{"id": '+str(global_settings["id"])+'}'
 
 @app.route("/set/id", methods=['POST'])
 def set_id():
+    req = request.form
+    global_settings['id'] = req["id"]
+    print(global_settings)
     return 'OK'
 
 @app.route("/get/mode", methods=['GET'])
 def get_mode():
-    return '{ "mode": "controller" }'
+    return '{"mode": '+str(global_settings["mode"])+'}'
 
 @app.route("/set/mode", methods=['POST'])
 def set_mode():
+    req = request.form
+    global_settings['mode'] = req["mode"]
+    print(global_settings)
     return 'OK'
 
 @app.route("/get/readings", methods=['GET'])
@@ -79,6 +111,13 @@ def set_interval():
 def get_interval():
     return '{"interval": "1"}'
 
+###################################################################################################
+# Main
+###################################################################################################
 if __name__ == '__main__':
+    global_settings = load_settings()
+    global_devices = load_devices()
+    print(global_settings)
+    print(global_devices)
     app.run(debug=True)
 
