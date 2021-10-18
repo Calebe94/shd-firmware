b6d5eb22cc3d297e272a85c5f0143f6da210aec0
calebe94 <calebe.sep@hotmail.com>
2021-10-18 16:48:58 -0300

    Fixed sensor interrupt

--- src/sensor/flowsensor.cpp
+++ src/sensor/flowsensor.cpp
@@ -27,13 +27,13 @@ static float litros = 0;
 void flowsensor_setup(void)
 {
     pinMode(FLOWSENSOR_GPIO, INPUT_PULLUP);
-    attachInterrupt(FLOWSENSOR_GPIO, flowsensor_isr_handler, RISING);
+    attachInterrupt(digitalPinToInterrupt(FLOWSENSOR_GPIO), flowsensor_isr_handler, RISING);
 }
 
 void flowsensor_init(void)
 {
-    flowsensor_queue = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(flowsensor_task, "flowsensor_task", 2048, NULL, 10, NULL);
+    //flowsensor_queue = xQueueCreate(10, sizeof(uint32_t));
+    //xTaskCreate(flowsensor_task, "flowsensor_task", 2048, NULL, 10, NULL);
     TimerHandle_t xFlowTimer = xTimerCreate("Timer", pdMS_TO_TICKS( 1000 ), pdTRUE, ( void * ) flowsensor_queue, flowsensor_timer_callback);
 
     if( xTimerStart( xFlowTimer, 0 ) != pdPASS )
@@ -46,8 +46,9 @@ void flowsensor_init(void)
 
 void IRAM_ATTR flowsensor_isr_handler()
 {
-    uint32_t gpio_num = (uint32_t) FLOWSENSOR_GPIO;
-    xQueueSendFromISR(flowsensor_queue, &gpio_num, NULL);
+    //uint32_t gpio_num = (uint32_t) FLOWSENSOR_GPIO;
+    //xQueueSendFromISR(flowsensor_queue, &gpio_num, NULL);
+    pulses++;
 }
 
 void flowsensor_task(void *arg)
